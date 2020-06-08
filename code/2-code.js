20bcb4ff42004980805b3baab8b641fda919e61e
xc9010 <xuchen199010@gmail.com>
2020-06-09 01:26:53 +0800

    add

---
+++ code/2-code.js
@@ -0,0 +1,16 @@
+
+function run() {
+    for(let i =0;i< 10;i++){
+        (function() {
+            setTimeout(() => console.log(i), 1000)
+        })()
+    }
+}
+
+function run1() {
+    for(var i=0;i<10;i++) {
+        (function(i) {
+            setTimeout(() => console.log(i), 1000*i)
+        })(i)
+    }
+}
